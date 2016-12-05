379fe5d8142658fbfa804f2f8f59dbd9edde927e
Admin_Liu <857319699@qq.com>
2016-12-05 10:12:06 +0800

    Initial Commit

---
+++ CustomUICollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomUICollectionView
+//
+//  Created by jyLiu on 2016/12/5.
+//  Copyright © 2016年 JY_L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomUICollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomUICollectionView
+//
+//  Created by jyLiu on 2016/12/5.
+//  Copyright © 2016年 JY_L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
